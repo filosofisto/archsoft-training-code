a7d160fdf6974626485cc52c1442e029153cd8e1
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-09-18 20:57:02 +0200

    conflict

--- hello-world/src/main/java/com/archsoft/Main.java
+++ hello-world/src/main/java/com/archsoft/Main.java
@@ -1,24 +1,19 @@
 package com.archsoft;
 
-import org.apache.commons.lang.StringUtils;
-
 import static java.lang.System.out;
 
 public class Main {
 
     // Comentario
     public static void main(String[] args) {
-        out.println("Hello Java");
+        // Mensagem para o usuario
+        out.println("Hello Java - Parabens 25 anos");
 
         if (args.length == 2) {
-            if (StringUtils.contains("1", 'a')) {
-
-            }
-
             Calculator calculator = new Calculator();
             int a = Integer.parseInt(args[0]);
             int b = Integer.parseInt(args[1]);
-            out.printf("%d + %d = %d", a, b, calculator.add(a, b));
+            out.printf("%s + %s = %s", a, b, calculator.add(a, b));
         }
     }
 }

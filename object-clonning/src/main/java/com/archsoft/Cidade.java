ed5438be67f2743164be09811345943fdacaa07d
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-09-30 19:12:21 +0200

    Formating code

--- object-clonning/src/main/java/com/archsoft/Cidade.java
+++ object-clonning/src/main/java/com/archsoft/Cidade.java
@@ -2,13 +2,13 @@ package com.archsoft;
 
 public class Cidade {
 
-	private String nome;
+    private String nome;
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- object-clonning/src/main/java/com/archsoft/Pessoa.java
+++ object-clonning/src/main/java/com/archsoft/Pessoa.java
@@ -2,26 +2,31 @@ package com.archsoft;
 
 public class Pessoa {
 
-	private String nome;
-	private int idade;
-	private Cidade cidade;
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public int getIdade() {
-		return idade;
-	}
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-	public Cidade getCidade() {
-		return cidade;
-	}
-	public void setCidade(Cidade cidade) {
-		this.cidade = cidade;
-	}
+    private String nome;
+    private int idade;
+    private Cidade cidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
 }

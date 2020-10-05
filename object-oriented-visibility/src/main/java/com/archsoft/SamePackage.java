55d9b3a8d921c79724251f073ee83f70ad83fd5e
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-10-05 19:39:32 +0200

    object-oriented-visibility (missing artifacts)

---
+++ object-oriented-visibility/src/main/java/com/archsoft/SamePackage.java
@@ -0,0 +1,15 @@
+package com.archsoft;
+
+public class SamePackage {
+
+    public void execute() {
+        Parent p = new Parent();
+
+        p.publicField = 12; // public visibility
+        p.protectedField = 11; // same package
+        p.packageField = 10; // same package
+
+        // Compile error: private visibility
+        // privateField = 13;
+    }
+}

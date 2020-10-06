1db478a8b6c4949c32931bef23a9a8ca8734aa50
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-10-06 23:38:11 +0200

    io-3 project

---
+++ io-3/src/main/java/com/archsoft/DataConsumer.java
@@ -0,0 +1,91 @@
+package com.archsoft;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class DataConsumer {
+
+    private File source;
+
+    private DataInputStream in;
+
+    private List<Integer> list;
+
+    private int total;
+
+    private int count;
+
+    public DataConsumer from(File source) {
+        this.source = source;
+        return this;
+    }
+
+    public DataConsumer prepare() throws FileNotFoundException {
+        prepareInput();
+        prepareList();
+        prepareStat();
+
+        return this;
+    }
+
+    private void prepareStat() {
+        total = 0;
+        count = 0;
+    }
+
+    private void prepareList() {
+        list = new ArrayList<>();
+    }
+
+    private void prepareInput() throws FileNotFoundException {
+        Objects.requireNonNull(source);
+        in = new DataInputStream(new FileInputStream(source));
+    }
+
+    public DataConsumer read() throws IOException {
+        Objects.requireNonNull(in);
+
+        int valor;
+
+        for (;;) {
+            try {
+                valor = in.readInt();
+
+                list.add(valor);
+                count++;
+                total += valor;
+            } catch (EOFException e) {
+                break;
+            }
+        }
+
+        return this;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int[] getData() {
+        Objects.requireNonNull(list);
+
+        int[] ret = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            ret[i] = list.get(i);
+        }
+
+        return ret;
+    }
+
+    public void close() throws IOException {
+        in.close();
+    }
+}
---
+++ io-3/src/main/java/com/archsoft/DataProducer.java
@@ -0,0 +1,52 @@
+package com.archsoft;
+
+import java.io.*;
+import java.util.Objects;
+import java.util.Random;
+
+public class DataProducer {
+
+    private File target;
+
+    private DataOutputStream out;
+
+    private Random random;
+
+    public DataProducer into(File target) {
+        this.target = target;
+
+        return this;
+    }
+
+    public DataProducer prepare() throws FileNotFoundException {
+        prepareOutput();
+        prepareRandom();
+
+        return this;
+    }
+
+    private void prepareRandom() {
+        random = new Random();
+    }
+
+    private void prepareOutput() throws FileNotFoundException {
+        Objects.requireNonNull(target);
+        out = new DataOutputStream(new FileOutputStream(target));
+    }
+
+    public DataProducer produce(int number) throws IOException {
+        Objects.requireNonNull(target);
+        Objects.requireNonNull(random);
+
+        for (int i = 0; i < number; i++) {
+            out.writeInt(random.nextInt(100)+1);
+        }
+
+        return this;
+    }
+
+    public void close() throws IOException {
+        Objects.requireNonNull(out);
+        out.close();
+    }
+}

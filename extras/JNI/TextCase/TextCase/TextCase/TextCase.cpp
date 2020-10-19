069b1fe17bb816aa5960b3279725949a809a5c2e
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-10-19 21:07:29 +0200

    Fix

--- extras/JNI/TextCase/TextCase/TextCase/TextCase.cpp
+++ extras/JNI/TextCase/TextCase/TextCase/TextCase.cpp
@@ -1,6 +1,4 @@
 #include "pch.h"
-#include <string>
-#include <algorithm>
 #include <jni.h>
 #include "com_archsoft_TextCase.h"
 
@@ -10,14 +8,14 @@ JNIEXPORT jstring JNICALL Java_com_archsoft_TextCase_upcase(JNIEnv* env, jobject
 {
 	// Step 1: Convert the JNI String (jstring) into C-String (char*)
 	const char* str = env->GetStringUTFChars(text, nullptr);
-	if (NULL == str) return nullptr;
+	if (nullptr == str) return nullptr;
 
 	// Step 2: release resources
 	env->ReleaseStringUTFChars(text, str);
 
 	// Step 3: Create a C-Style string with upper case
 	size_t size = strlen(str) + 1;
-	char* out = new char[strlen(str) + 1];
+	char* out = new char[size];
 	for (unsigned int i = 0; i < size; i++) {
 		out[i] = toupper(str[i]);
 	}
@@ -37,7 +35,7 @@ JNIEXPORT jstring JNICALL Java_com_archsoft_TextCase_lowcase(JNIEnv* env, jobjec
 
 	// Step 3: Create a C-Style string with upper case
 	size_t size = strlen(str) + 1;
-	char* out = new char[strlen(str) + 1];
+	char* out = new char[size];
 	for (unsigned int i = 0; i < size; i++) {
 		out[i] = tolower(str[i]);
 	}

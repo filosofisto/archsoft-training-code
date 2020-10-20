db2e602205c0379fcc6ab3e1e6241dac0c6028b7
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-10-21 00:43:04 +0200

    lambda-1 project

---
+++ lambda-1/src/main/java/com/archsoft/Main.java
@@ -0,0 +1,51 @@
+package com.archsoft;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User u1 = new User("Rodrigo", 75, false);
+        User u2 = new User("Jeronimo", 79, true);
+        User u3 = new User("Francesca", 82, false);
+
+        List<User> users = Arrays.asList(u1, u2, u3);
+
+//        1 - Old style
+//        for (User user: users) {
+//            System.out.println(user.getName());
+//        }
+
+//        2 - forEach + Consumer
+//        users.forEach(new Consumer<User>() {
+//            @Override
+//            public void accept(User user) {
+//                System.out.println(user.getName());
+//            }
+//        });
+
+//        3 - Lambda
+//        Consumer<User> shower = (User user) -> {
+//            System.out.println(user.getName());
+//        };
+//        users.forEach(shower);
+
+//        4 - Lambda (ocultando o tipo do parametro)
+//        Consumer<User> shower = user -> {
+//            System.out.println(user.getName());
+//        };
+//        users.forEach(shower);
+
+//        5 - Lambda (removendo colchetes - apenas 1 comando)
+//        Consumer<User> shower = user -> System.out.println(user.getName());
+//        users.forEach(shower);
+
+//        6 - Lambda: versao mais otimizada
+//        users.forEach(user -> System.out.println(user.getName()));
+
+//        7 - Reference Method
+        users.forEach(User::print);
+    }
+}

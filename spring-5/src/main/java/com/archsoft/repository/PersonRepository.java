719def51d66cef062b39730062bd095eac8c0498
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-11-02 00:03:00 +0100

    spring-5 project

---
+++ spring-5/src/main/java/com/archsoft/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.archsoft.repository;
+
+import com.archsoft.model.Person;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, Long> {
+}

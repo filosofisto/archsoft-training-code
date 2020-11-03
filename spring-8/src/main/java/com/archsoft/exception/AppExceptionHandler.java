f43fa3ca688d4886771fa4768484690d29946e5b
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-11-04 00:23:03 +0100

    spring-8 update

--- spring-8/src/main/java/com/archsoft/exception/AppExceptionHandler.java
+++ spring-8/src/main/java/com/archsoft/exception/AppExceptionHandler.java
@@ -1,18 +1,19 @@
 package com.archsoft.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.time.LocalDateTime;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
 @RestController
@@ -32,5 +33,16 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
                         .status(NOT_FOUND.value())
                         .build(),
                 NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>(
+                ExceptionResponse.builder()
+                        .date(LocalDateTime.now())
+                        .message("Trocando os metodos HTTP? " + ex.getMessage())
+                        .status(METHOD_NOT_ALLOWED.value())
+                        .build(),
+                METHOD_NOT_ALLOWED);
     }
 }

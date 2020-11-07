8cb1e4f5de60d7471a3cd40c5ec6aa31f0b76e26
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-11-07 20:06:38 +0100

    Atualizacao do JNI para o Ubuntu Mate

--- extras/JNI/spring-jni-monitor/gen_header.sh
+++ extras/JNI/spring-jni-monitor/gen_header.sh
@@ -3,7 +3,7 @@
 echo "Generate header files from Java Class"
 cd target/classes
 rm com_archsoft_StatisticCollector.h
-/usr/lib64/jvm/jdk1.8.0_251/bin/javah -jni com.archsoft.StatisticCollector
+/usr/lib/jvm/java-8-openjdk-amd64/bin/javah -jni com.archsoft.StatisticCollector
 cat com_archsoft_StatisticCollector.h
 mv com_archsoft_StatisticCollector.h ../../../stat/com_archsoft_StatisticCollector.h
 cd ..

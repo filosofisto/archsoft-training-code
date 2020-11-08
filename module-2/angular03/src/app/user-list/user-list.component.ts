ec0d7dfcd35acf5ec925100430214a1dda74acdd
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-11-08 21:59:17 +0100

    angular03 project

---
+++ module-2/angular03/src/app/user-list/user-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  names: string[];
+
+  constructor() {
+    this.names = ['Hanna', 'Eduardo', 'Hiago'];
+  }
+
+  ngOnInit(): void {
+  }
+
+}

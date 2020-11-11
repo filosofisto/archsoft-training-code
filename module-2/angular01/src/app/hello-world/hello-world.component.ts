a8d382830ea3607fda02493f78f15fcd530b1429
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-11-11 22:17:53 +0100

    Updates from class

--- module-2/angular01/src/app/hello-world/hello-world.component.ts
+++ module-2/angular01/src/app/hello-world/hello-world.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hello-world',
   templateUrl: './hello-world.component.html',
-  // template: `
-  //   <h3>Hello World inline (template)</h3>
-  // `,
+  /*template: `
+  <h3>Hello World inline (template)</h3>
+  <small>Template created with template attribute</small>
+  `,*/
   styleUrls: ['./hello-world.component.css']
 })
 export class HelloWorldComponent implements OnInit {

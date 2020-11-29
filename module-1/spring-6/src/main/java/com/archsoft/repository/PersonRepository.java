8537f8a4fd7a062d4399dc0df13fc6208658412b
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-11-29 17:50:53 +0100

    Updates

--- module-1/spring-6/src/main/java/com/archsoft/repository/PersonRepository.java
+++ module-1/spring-6/src/main/java/com/archsoft/repository/PersonRepository.java
@@ -1,9 +1,14 @@
 package com.archsoft.repository;
 
 import com.archsoft.model.Person;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonRepository extends CrudRepository<Person, Long> {
+
+    @Query("select p from Person p order by p.name")
+    @Override
+    Iterable<Person> findAll();
 }

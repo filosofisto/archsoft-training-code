a21b2618e4d83805d84fda03e0c27ddd179793a6
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2020-12-02 18:51:30 +0100

    Fix show error message when try to connect but it can't

--- module-2/angular26-spring06/src/app/components/person/person-view/person-view.component.ts
+++ module-2/angular26-spring06/src/app/components/person/person-view/person-view.component.ts
@@ -12,8 +12,6 @@ export class PersonViewComponent implements OnInit {
 
   people: Person[] = [];
 
-  // person: Person;
-
   constructor(private peopleService: PeopleService,
               private messageNotificationService: MessageNotificationService) { }
 
@@ -22,10 +20,7 @@ export class PersonViewComponent implements OnInit {
   }
 
   loadPeople(): void {
-    console.log('loadPeople');
-    this.peopleService.list().subscribe(
-      data => this.people = data,
-        error => this.messageNotificationService.notifyError(error.message));
+    this.peopleService.list().subscribe(data => this.people = data);
   }
 
   save(person: Person): void {
@@ -38,12 +33,9 @@ export class PersonViewComponent implements OnInit {
 
   private create(person: Person): void {
     this.peopleService.create(person).subscribe(
-      data => {
+      () => {
         this.messageNotificationService.notifySuccess('Person created');
         this.loadPeople();
-      },
-      error => {
-        this.messageNotificationService.notifyError(error.message);
       }
     );
   }
@@ -53,9 +45,6 @@ export class PersonViewComponent implements OnInit {
       () => {
         this.messageNotificationService.notifySuccess('Person updated');
         this.loadPeople();
-      },
-      error => {
-        this.messageNotificationService.notifyError(error.message);
       }
     );
   }
@@ -65,18 +54,10 @@ export class PersonViewComponent implements OnInit {
       () => {
         this.messageNotificationService.notifyWarn(`Person ${person.name} removed successful`);
         this.loadPeople();
-      },
-      error => this.messageNotificationService.notifyError(error.message)
-      );
+      });
   }
 
   read(person: Person): void {
-    this.peopleService.read(person.id).subscribe(
-      data => {
-        // this.person = data;
-        this.peopleService.notify(data);
-      },
-      error => this.messageNotificationService.notifyError(error.message)
-    );
+    this.peopleService.read(person.id).subscribe(data => this.peopleService.notify(data));
   }
 }

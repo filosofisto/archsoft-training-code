827f88a77700ef0d3989af475e56c8bfe5a6af9a
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2021-05-05 07:04:38 +0200

    kafka-1 project

---
+++ module-5/kafka/kafka-1/README.md
@@ -0,0 +1,10 @@
+# Projeto Kafka-1
+
+##Objetivo
+- KafkaProducer
+
+##Instruções
+
+    su -l kafka
+    cd kafka/bin/
+    /kafka-console-consumer.sh --bootstrap-server 127.0.0.1:9092 --topic first_topic --group my-first-application
--- module-5/kafka/kafka-1/src/main/java/com/archsoft/kafka/ProducerDemo.java
+++ module-5/kafka/kafka-1/src/main/java/com/archsoft/kafka/ProducerDemo.java
@@ -1,4 +1,35 @@
 package com.archsoft.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+
+    public static void main(String[] args) {
+        // Create properties
+        Properties properties = new Properties();
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // Create ProducerRecord
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "Hello World");
+
+        // Send data asynchronous
+        producer.send(record);
+
+        // Flush Producer
+        producer.flush();
+
+        // Close Producer
+        producer.close();
+    }
 }

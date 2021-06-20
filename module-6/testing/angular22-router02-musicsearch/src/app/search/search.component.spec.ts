bd560f8d0ebbd949c4242b23bdb2826fbd4ee6a8
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2021-06-20 17:03:07 +0200

    SearchComponent unit test

--- module-6/testing/angular22-router02-musicsearch/src/app/search/search.component.spec.ts
+++ module-6/testing/angular22-router02-musicsearch/src/app/search/search.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SearchComponent } from './search.component';
+import {SearchComponent} from './search.component';
 import {RouterModule} from '@angular/router';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {SpotifyService} from '../spotify.service';
@@ -11,8 +11,8 @@ describe('SearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchComponent ],
       imports: [RouterModule.forRoot([]), HttpClientTestingModule],
+      declarations: [SearchComponent],
       providers: [
         SpotifyService
       ]
@@ -28,5 +28,34 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('empty results', () => {
+    component.results = [];
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.test').textContent).toContain('No tracks were found with the term');
+  });
+
+  it('not empty results', () => {
+    component.results = [
+      {
+        id: 1,
+        name: 'Matallica',
+        album: {
+          id: 1,
+          images: [
+            { url: 'some address' }
+          ]
+        },
+        artists: [
+          { id: 1, name: 'Metallica'}
+        ]
+      }
+    ];
+
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('img').attributes.getNamedItem('src').value).toBe('some address');
   });
 });
